f41324a4072b25006d21552787b6dda29508b2e6
buixuanbach1512 <buixuanbach102@gmail.com>
2023-12-28 15:08:03 +0700

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,12 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: ["https://frontend-fzzkq055r-buixuanbach1512s-projects.vercel.app"] }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://frontend-fzzkq055r-buixuanbach1512s-projects.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 app.use(cookieParser());
 app.use(morgan('dev'));
 
